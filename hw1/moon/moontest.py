b4546183dcdc6f39046a011d4cee1efd7e0c7988
Warren MacEvoy <wmacevoy@gmail.com>
2021-02-04 11:43:04 -0800

    run tests from command line

--- hw1/moon/moontest.py
+++ hw1/moon/moontest.py
@@ -22,5 +22,6 @@ class MoonTest(unittest.TestCase):
         self.assertEqual(moon.size,size)
         moon.cycle()
         self.assertEqual(moon.phase,phase+1)
-
-
+        
+if __name__ == '__main__':
+    unittest.main()
